574f2036dc81f803965346c79230f737ed1456e7
Doanta Ginting <doantaaloycius@gmail.com>
2024-07-15 23:49:53 +0700

    Feat: add tailwind dependency, create navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,60 @@
+<header class="px-4 py-6 mx-auto shadow-md">
+    <div class="flex items-center justify-between container relative">
+        <a class="font-bold" href="">Mitra Property</a>
+        <nav>
+            <ul class="flex items-center space-x-5 font-light">
+                <li class="group relative">
+                    <button class="hover:opacity-50 group">Tipe Property
+                        <span
+                            class="icon-[ep--arrow-down] my-auto ms-1  transition-opacity duration-300 absolute mt-1.5 group-hover:opacity-0"></span>
+                        <span
+                            class="icon-[ep--arrow-up] my-auto ms-1  transition-opacity duration-300 opacity-0 mt-1.5 group-hover:opacity-100"></span>
+                    </button>
+                    <div
+                        class="absolute left-0 w-96 bg-white border border-gray-200 mt-2 hidden group-hover:block">
+                        <div class="container mx-auto px-4 py-6">
+                            <div class="grid grid-cols-3 gap-6">
+                                <div>
+                                    <h3 class="font-bold mb-4">Category 1</h3>
+                                    <ul class="space-y-2">
+                                        <li><a href="#" class="hover:underline">Sub-item 1</a></li>
+                                        <li><a href="#" class="hover:underline">Sub-item 2</a></li>
+                                        <li><a href="#" class="hover:underline">Sub-item 3</a></li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h3 class="font-bold mb-4">Category 2</h3>
+                                    <ul class="space-y-2">
+                                        <li><a href="#" class="hover:underline">Sub-item 1</a></li>
+                                        <li><a href="#" class="hover:underline">Sub-item 2</a></li>
+                                        <li><a href="#" class="hover:underline">Sub-item 3</a></li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h3 class="font-bold mb-4">Category 3</h3>
+                                    <ul class="space-y-2">
+                                        <li><a href="#" class="hover:underline">Sub-item 1</a></li>
+                                        <li><a href="#" class="hover:underline">Sub-item 2</a></li>
+                                        <li><a href="#" class="hover:underline">Sub-item 3</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="flex cursor-pointer hover:opacity-50">Layanan<span
+                        class="icon-[ep--arrow-down] my-auto ms-1 "></span></li>
+                <li class="cursor-pointer hover:opacity-50">About Us</li>
+                <li class="cursor-pointer hover:opacity-50">Blog</li>
+            </ul>
+        </nav>
+        <nav>
+            <ul>
+                <li class="bg-red-500 cursor-pointer rounded-lg px-4 py-2 font-semibold text-white">
+                    Contact Us
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>

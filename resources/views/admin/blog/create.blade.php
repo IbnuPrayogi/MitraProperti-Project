35427677f9e3c61c61d77a66c2c25451cb1f3d0d
IbnyPrayogi <muhammadibnuprayogi24@gmail.com>
2025-05-09 22:40:15 +0700

    fix copy writing in admin

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -8,7 +8,7 @@
                 <!-- general form elements -->
                 <div class="card card-primary">
                     <div class="card-header">
-                        <h3 class="card-title">Add New Property</h3>
+                        <h3 class="card-title">Add New Blog</h3>
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
